41a5f267e4c4f44a67b34a2bb26d2068589255b6
Hui Yang <1191759350@qq.com>
2025-06-05 22:27:45 +0800

    commit

--- app/src/main/java/com/yuwjoo/novelapp/utils/dsbridge/jsapi/BaseJSApi.java
+++ app/src/main/java/com/yuwjoo/novelapp/utils/dsbridge/jsapi/BaseJSApi.java
@@ -7,49 +7,79 @@ import androidx.annotation.NonNull;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
+import com.yuwjoo.novelapp.okhttp.OkHttpUtils;
 import com.yuwjoo.novelapp.utils.dsbridge.module.RequestOptions;
 
 import org.json.JSONException;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
 import okhttp3.MediaType;
 import okhttp3.Request;
 import okhttp3.RequestBody;
+import okhttp3.Response;
 import wendu.dsbridge.CompletionHandler;
 
 public class BaseJSApi {
 
     @JavascriptInterface
-    public void request(@NonNull Object object, CompletionHandler<Object> handler) throws JSONException {
+    public void request(@NonNull Object object, CompletionHandler<Object> handler) {
         RequestOptions requestOptions = new Gson().fromJson(object.toString(), RequestOptions.class); // 请求配置
-        Set<Map.Entry<String, JsonElement>> headerEntrySet = requestOptions.getHeaders().entrySet(); // 请求头set
         String contentType = "application/json"; // body数据类型
         Map<String, Object> responseData = new HashMap<>(); // 响应数据
         Request.Builder rb = new Request.Builder();
 
-        rb.url(requestOptions.getUrl()); // 设置url
-
-        for (Map.Entry<String, JsonElement> entry : headerEntrySet) {
-            String key = entry.getKey();
-            String value = String.valueOf(entry.getValue());
-            if (key.equalsIgnoreCase("content-type")) {
-                contentType = value;
+        HttpUrl.Builder httpUrlBuilder = Objects.requireNonNull(HttpUrl.parse(requestOptions.getUrl())).newBuilder();
+        if (requestOptions.getParams() != null) {
+            Set<Map.Entry<String, JsonElement>> paramsEntrySet = requestOptions.getParams().entrySet(); // 请求头set
+            for (Map.Entry<String, JsonElement> entry : paramsEntrySet) {
+                String key = entry.getKey();
+                String value = String.valueOf(entry.getValue());
+                httpUrlBuilder.addQueryParameter(key, value); //设置查询参数
             }
-            rb.header(key, value); //设置请求头
+        }
+        rb.url(httpUrlBuilder.build()); // 设置url
+
+        if (requestOptions.getHeaders() != null) {
+            Set<Map.Entry<String, JsonElement>> headerEntrySet = requestOptions.getHeaders().entrySet(); // 请求头set
+            for (Map.Entry<String, JsonElement> entry : headerEntrySet) {
+                String key = entry.getKey();
+                String value = String.valueOf(entry.getValue());
+                if (key.equalsIgnoreCase("content-type")) {
+                    contentType = value;
+                }
+                rb.header(key, value); //设置请求头
+            }
         }
 
+        RequestBody requestBody = null;
         if (requestOptions.getData() != null) {
-            RequestBody requestBody = RequestBody
+            requestBody = RequestBody
                     .create(requestOptions.getData().toString(), MediaType.parse(contentType));
-            if (requestOptions.getMethod().equalsIgnoreCase("post")) {
-                rb.post(requestBody); // 设置请求body
-            } else {
-                rb.put(requestBody); // 设置请求body
-            }
         }
+        rb.method(requestOptions.getMethod(), requestBody); // 设置请求方法和请求体
+
+        Call call = OkHttpUtils.getOkHttpClient().newCall(rb.build());
+
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+
+            }
+        }); // 发送http请求
+
 
 //        headerEntrySet.
 
